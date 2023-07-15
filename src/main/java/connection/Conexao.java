10ae48d08c247af1c3f675af2cb0f67764f6a27e
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-07-14 22:24:20 -0300

    primeiro commit

---
+++ src/main/java/connection/Conexao.java
@@ -0,0 +1,17 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {   public static Connection getConnection(){
+    try{
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/AResistencia",
+                "postgres", "123456");
+        return connection;
+    }catch (SQLException e){
+        e.printStackTrace();
+        return null;
+    }
+}
+}
